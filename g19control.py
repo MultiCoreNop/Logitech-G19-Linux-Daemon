29494bf2509ab424f9916415147daa27583ff2aa
MultiCoreNOP <multicorenop@gmail.com>
2010-04-11 16:28:40 +0200

    refactored everything into a class

--- g19control.py
+++ g19control.py
@@ -1,86 +1,91 @@
+import time
 import usb
 
-def find_device(idVendor, idProduct):
-    for bus in usb.busses():
-        for dev in bus.devices:
-            if dev.idVendor == idVendor and \
-                    dev.idProduct == idProduct:
-                return dev
-    return None
+class LogitechG19(object):
 
-def open_lcd_control(dev):
-    config = dev.configurations[0]
-    iface = config.interfaces[1][0]
-    ep = iface.endpoints[0]
+    def __init__(self):
+        self.__device = self._find_device(0x046d, 0xc229)
+        if not self.__device:
+            raise usb.USBError("G19 not found on USB bus")
+        self.__handle = self.__open_handle(self.__device)
 
-    handle = dev.open()
-    try:
-        handle.detachKernelDriver(iface)
-    except usb.USBError:
-        pass
-    handle.setConfiguration(config)
-    handle.claimInterface(iface)
-    return handle
+    @staticmethod
+    def _find_device(idVendor, idProduct):
+        for bus in usb.busses():
+            for dev in bus.devices:
+                if dev.idVendor == idVendor and \
+                        dev.idProduct == idProduct:
+                    return dev
+        return None
 
-def open_lcd_display(dev):
-    config = dev.configurations[0]
-    iface = config.interfaces[0][0]
+    @staticmethod
+    def __open_handle(dev):
+        handle = dev.open()
+        return handle
 
-    handle = dev.open()
-    try:
-        handle.detachKernelDriver(iface)
-    except usb.USBError:
-        pass
-    handle.setConfiguration(config)
-    handle.claimInterface(iface)
-    return handle
+    def __use_lcd_control(self):
+        try:
+            self.__handle.releaseInterface()
+        except ValueError:
+            pass
+        config = self.__device.configurations[0]
+        iface = config.interfaces[1][0]
+        try:
+            self.__handle.detachKernelDriver(iface)
+        except usb.USBError:
+            pass
+        self.__handle.setConfiguration(config)
+        self.__handle.claimInterface(iface)
 
-def set_bg_color(dev, r, g, b):
-    handle = open_lcd_control(dev)
-    rtype = usb.TYPE_CLASS | usb.RECIP_INTERFACE
-    colorData = [7, r, g, b]
-    handle.controlMsg(rtype, 0x09, colorData, 0x307, 0x01)
+    def __use_lcd_display(self):
+        try:
+            self.__handle.releaseInterface()
+        except ValueError:
+            pass
+        config = self.__device.configurations[0]
+        iface = config.interfaces[0][0]
+        try:
+            self.__handle.detachKernelDriver(iface)
+        except usb.USBError:
+            pass
+        self.__handle.setConfiguration(config)
+        self.__handle.claimInterface(iface)
 
-def set_display_brightness(dev, val):
-    '''val in [0,100]'''
-    data = [val, 0xe2, 0x12, 0x00, 0x8c, 0x11, 0x00, 0x10, 0x00]
-    rtype = usb.TYPE_VENDOR | usb.RECIP_INTERFACE
-    handle.controlMsg(rtype, 0x0a, data, 0x0, 0x0)
+    def set_bg_color(self, r, g, b):
+        self.__use_lcd_control()
+        rtype = usb.TYPE_CLASS | usb.RECIP_INTERFACE
+        colorData = [7, r, g, b]
+        self.__handle.controlMsg(rtype, 0x09, colorData, 0x307, 0x01)
 
-def set_display_color(dev, b1, b2):
-    handle = open_lcd_display(dev)
-    frame = [0x10, 0x0F, 0x00, 0x58, 0x02, 0x00, 0x00, 0x00,
-             0x00, 0x00, 0x00, 0x3F, 0x01, 0xEF, 0x00, 0x0F]
-    for i in range(16, 256):
-        frame.append(i)
-    for i in range(256):
-        frame.append(i)
-    for i in range(320 * 240):
-        frame.append(b1)
-        frame.append(b2)
-    handle.bulkWrite(2, frame)
+    def set_display_brightness(self, val):
+        '''val in [0,100]'''
+        self.__use_lcd_control()
+        data = [val, 0xe2, 0x12, 0x00, 0x8c, 0x11, 0x00, 0x10, 0x00]
+        rtype = usb.TYPE_VENDOR | usb.RECIP_INTERFACE
+        self.__handle.controlMsg(rtype, 0x0a, data, 0x0, 0x0)
+
+    def set_display_color(self, b1, b2):
+        self.__use_lcd_display()
+        frame = [0x10, 0x0F, 0x00, 0x58, 0x02, 0x00, 0x00, 0x00,
+                 0x00, 0x00, 0x00, 0x3F, 0x01, 0xEF, 0x00, 0x0F]
+        for i in range(16, 256):
+            frame.append(i)
+        for i in range(256):
+            frame.append(i)
+        for i in range(320 * 240):
+            frame.append(b1)
+            frame.append(b2)
+        # on avg. only every 2nd call succeeds - dunno why
+        while True:
+            try:
+                self.__handle.bulkWrite(2, frame, 100)
+                break
+            except usb.USBError:
+                time.sleep(0.01)
 
 def main():
     pass
 
 # if __name__ == '__main__':
 #     main()
-
-dev = find_device(0x046d, 0xc229)
-if dev:
-    #set_bg_color(dev, 255, 255, 0)
-    #handle = open_lcd_control(dev)
-
-    config = dev.configurations[0]
-    iface = config.interfaces[1][0]
-
-    handle = dev.open()
-    handle.reset()
-    try:
-        handle.detachKernelDriver(iface)
-    except usb.USBError as (e):
-        print "usb error: ", str(e)
-    handle.setConfiguration(config)
-    handle.claimInterface(iface)
-
-    rtype = usb.TYPE_CLASS | usb.RECIP_INTERFACE
+lg19 = LogitechG19()
